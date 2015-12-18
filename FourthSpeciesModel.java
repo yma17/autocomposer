de5e2e30b896cfc0e9384c74133b4cc10bbdefb0
yma17 <yma17@cranbrook.edu>
2015-12-17 21:31:40 -0500

    .fourthspeciesmodel

---
+++ FourthSpeciesModel.java
@@ -0,0 +1,20 @@
+public class FourthSpeciesModel extends Model
+{
+    private boolean[] suspensions; //each measure, suspensions? Yes or No?
+    public FourthSpeciesModel()
+    {
+        super();
+    }
+    public FourthSpeciesModel(String key, String mode, int measures)
+    {
+        super(key, mode, measures);
+    }
+    public String toString()
+    {
+        return super.toString();
+    }
+    private void determineSuspensionStructure()
+    {
+        //insert code here
+    }
+}
