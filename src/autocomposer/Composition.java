7a0d6ddaec7608e1ba815a81c6b5062b575a9bc9
Eric Ma <yma17@cranbrook.edu>
2016-02-27 15:33:24 -0500

    attempts with findMIDIInterval
    (unsuccessful)

--- src/autocomposer/Composition.java
+++ src/autocomposer/Composition.java
@@ -44,6 +44,61 @@ public class Composition implements NotesAndKeys
     	}
     	return pitch;
     }
+    public int findMIDIInterval(int interval) {
+    	/*
+    	int x = 0;
+    	boolean b = true;
+    	int a = model.getModeValue()+(interval-1); //to facilitate method- avoid outofBoundsException
+    	*/
+    	/*
+    	if(a <= 0) {
+    		x = -1;
+    		a += 9; 
+    	}
+    	if(a > 7) {
+    		if(a == 8 || a == 9)
+    			x = 0;
+    		else
+    			x = 1;
+    		a -= 7;
+    	}
+    	*/
+    	/*
+    	if(a < -1) {
+    		x = -1;
+    	    a += 9;
+    	}
+    	if(a == -1)
+    	
+		return (DIATONIC_INTERVALS[a]+12*x) - DIATONIC_INTERVALS[model.getModeValue()];
+		*/
+        /*
+    	int a = (interval-1) + model.getModeValue();
+        int x = 0;
+        if(a < 0) {
+        	while(a < 0) {
+        		x--;
+        		a += 7;
+        	}
+        }
+        else if(a >= 7) {
+        	while(a >= 7) {
+        		x++;
+        		a -= 7;
+        	}
+        }
+        return (DIATONIC_INTERVALS[a]+12*x) - DIATONIC_INTERVALS[model.getModeValue()];
+        */
+    	/*
+    	String s = model.getMode();
+    	if(s.equals("Ionian")) {
+    		if(interval < 0)
+    			return IONIAN_DIATONIC_INTERVALS[7+interval+1] - 12;
+    		else
+    			return IONIAN_DIATONIC_INTERVALS[interval-1];
+        */
+        
+    }
     public Note[] composeCantusFirmus() //composes the cantus firmus
     {
         Note[] cantusFirmus = new Note[model.getMeasures()];
@@ -60,16 +115,16 @@ public class Composition implements NotesAndKeys
         	int tonicToLowPointInterval = this.determineTonicToLPInterval();
         	int leapToFPInterval = this.determineLeapToFPInterval(model.getMode(), tonicToLowPointInterval);
         	
-        	int tonicToFPInterval = leapToFPInterval - tonicToLowPointInterval + 1;
-        	cantusFirmus[focalPoint - 1] = new Note(this.findPitch(model.getKey()) + DIATONIC_INTERVALS[model.getModeValue()%7 + tonicToFPInterval - 1]); //compose focal point
-        	
+        	int tonicToFPInterval = leapToFPInterval + tonicToLowPointInterval + 1;
+        	cantusFirmus[focalPoint - 1] = new Note(findPitch(model.getKey()) + this.findMIDIInterval(tonicToFPInterval)); //compose focal point
+
 			int lowPoint = focalPoint - 1;
         	if(leapToFPInterval == 8 && focalPoint >= 6) { //2-interval leap (1-interval if mode and tonic to low pt interval do not permit)
         		double x = Math.random();
         		if(x < 0.2) { //5th + 4th
         			boolean b = this.checkFifthPlusFourth(model.getMode(), tonicToLowPointInterval);
         			if(b) {
-        		        cantusFirmus[lowPoint - 2] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue()%7 + tonicToLowPointInterval - 1]); //compose low point
+        		        cantusFirmus[lowPoint - 2] = new Note(this.findPitch(model.getKey()) + this.findMIDIInterval(tonicToLowPointInterval)); //compose low point
         	            cantusFirmus[lowPoint - 1] = new Note(cantusFirmus[focalPoint - 1].getPitch() + 7);
         			}
         		}
@@ -77,11 +132,11 @@ public class Composition implements NotesAndKeys
         			//cantusFirmus[focalPoint - 2] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[tonicToLowPointInterval - 1]); //compose low point
         			//TODO
         		//}
-        		cantusFirmus[lowPoint - 1] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue()%7 + tonicToLowPointInterval - 1]); //compose low point
+        		cantusFirmus[lowPoint - 1] = new Note(this.findPitch(model.getKey()) + this.findMIDIInterval(tonicToLowPointInterval)); //compose low point
         		lowPoint--;
         	}
         	else //single leap
-        		cantusFirmus[lowPoint - 1] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue()%7 + tonicToLowPointInterval - 1]); //compose low point 	
+        		cantusFirmus[lowPoint - 1] = new Note(this.findPitch(model.getKey()) + this.findMIDIInterval(tonicToLowPointInterval)); //compose low point 	
         }
         /*
         else if(preFPContourType == 2) {
@@ -96,23 +151,26 @@ public class Composition implements NotesAndKeys
 		//Note[] preFP = this.composePreFP(focalPoint, preFPContourType);
         
         //determine 2nd note
+        /*
         double w = Math.random();
         if(w < 0.5)
-        	cantusFirmus[1] = new Note(this.findPitch(model.getKey()) + DIATONIC_INTERVALS[model.getModeValue()%7 + determineSecondNote(w) - 1]);
+        	cantusFirmus[1] = new Note(this.findPitch(model.getKey()) + DIATONIC_INTERVALS[model.getModeValue() + determineSecondNote(w) - 1] - DIATONIC_INTERVALS[model.getModeValue()]);
         else
-        	cantusFirmus[1] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue()%7 + determineSecondNote(w) - 1]);
+        	cantusFirmus[1] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue() + determineSecondNote(w) - 1] - DIATONIC_INTERVALS[model.getModeValue()]);
+        */
         
         //determine pre focal point
         for(int x = 2; x < focalPoint - 3; x++) {
         	//cantusFirmus[x] = composeNextNote(x, lastInterval, twoIntervalsBefore, lastDirection, twoDirectionsBefore); //TODO
         }
         
+        
         //determine 2nd to last note
         double z = Math.random();
         if(z < 0.5)
-        	cantusFirmus[cantusFirmus.length - 2] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue()%7 + 1]); //step up
+        	cantusFirmus[cantusFirmus.length - 2] = new Note(this.findPitch(model.getKey()) - DIATONIC_INTERVALS[model.getModeValue() + 1] - DIATONIC_INTERVALS[model.getModeValue()]); //step up
         else
-        	cantusFirmus[cantusFirmus.length - 2] = new Note(this.findPitch(model.getKey()) + DIATONIC_INTERVALS[model.getModeValue()%7 + 1]); //step down
+        	cantusFirmus[cantusFirmus.length - 2] = new Note(this.findPitch(model.getKey()) + DIATONIC_INTERVALS[model.getModeValue() + 1] - DIATONIC_INTERVALS[model.getModeValue()]); //step down
         
         //@SuppressWarnings("unused")
 		//Note[] postFP = this.composePostFP(model.getMeasures(), focalPoint, postFPContourType);
@@ -160,6 +218,7 @@ public class Composition implements NotesAndKeys
     	}
     }
     private int determinePreFPContour() { //helper method of composeCantusFirmus. Determines the contour from the first note to the FP.
+    	/*
     	double x = Math.random();
     	if(x < 0.3333)
     		return 1; //Type 1 = go down to low point and leap to focal point
@@ -167,6 +226,8 @@ public class Composition implements NotesAndKeys
     		return 2; //Type 2 = travel, return to tonic and leap to focal point
     	else
     		return 3; //Type 3 = gradual motion upwards towards focal point
+    	*/
+    	return 1;
     }
     private int determinePostFPContour() { //helper method of composeCantusFirmus. Determines the contour from the FP to the last note.
     	double x = Math.random();
@@ -177,7 +238,7 @@ public class Composition implements NotesAndKeys
     }
     private int determineTonicToLPInterval() { //Case I helper method
     	double x = Math.random();
-    	return (int)(x * 4.0) + 2; //2 = 2nd, 3 = 3rd, etc. (will return 2-5)
+    	return (int)(x * 4.0) - 5; //-2 = 2nd, -3 = 3rd, etc. (will return -2-(-5))
     }
     private int determineLeapToFPInterval(String mode, int tonicToLP) { //Case I helper method
     	double x = Math.random();
@@ -216,9 +277,7 @@ public class Composition implements NotesAndKeys
     	return true;
     }
     private int determineSecondNote(double w) { //returns 1-2 interval
-    	if(w >= 0.5)
-    		w -= 0.5;
-    	return (int)w*8 + 2; //returns number between 2 and 5
+    	return (int)w*4 + 2; //returns number between 2 and 5
     }
     /*
     public Note[] composeNoteByNote(int notesToBeComposed, Note[] previousNotes, ArrayList<Note> futureNotes) { //composes all notes in an empty section
--- src/testing/AutocomposerTester.java
+++ src/testing/AutocomposerTester.java
@@ -12,8 +12,19 @@ public class AutocomposerTester implements NotesAndKeys
 
 	public static void main(String[] args)
 	{
-		Model m = new Model("C","Ionian",9);
-		Composition composer = new Composition(m);
+		Model a = new Model("C","Ionian",9);
+		Model b = new Model("D","Dorian",9);
+		Model c = new Model("E","Phrygian",9);
+		Model d = new Model("F","Lydian",9);
+		Model e = new Model("G","Mixolydian",9);
+		Model f = new Model("A","Aeolian",9);
+		
+		Composition composer1 = new Composition(a);
+		Composition composer2 = new Composition(b);
+		Composition composer3 = new Composition(c);
+		Composition composer4 = new Composition(d);
+		Composition composer5 = new Composition(e);
+		Composition composer6 = new Composition(f);
 		
 		/*
 		int x = composer.findPitch(m.getKey()) + DIATONIC_INTERVALS[4];
@@ -22,13 +33,50 @@ public class AutocomposerTester implements NotesAndKeys
 		System.out.println(n.midiValue());
 		*/
 		
-		Note[] array = composer.composeCantusFirmus();
-		
-		for(int x = 0; x < array.length; x++) {
-			if(array[x] != null) {
-				System.out.println(x + " " + array[x].midiValue());
-			}
+		//System.out.println(composer.findPitch("G-flat"));
+		Note[] array1 = composer1.composeCantusFirmus();
 
+		for(int x = 0; x < array1.length; x++) {
+			if(array1[x] != null) {
+				System.out.println(x + " " + array1[x].midiValue());
+			}
+		}
+		
+		System.out.println();
+		Note[] array2 = composer2.composeCantusFirmus();
+		for(int x = 0; x < array2.length; x++) {
+			if(array2[x] != null) {
+				System.out.println(x + " " + array2[x].midiValue());
+			}
+		}
+		System.out.println();
+		Note[] array3 = composer3.composeCantusFirmus();
+		for(int x = 0; x < array3.length; x++) {
+			if(array3[x] != null) {
+				System.out.println(x + " " + array3[x].midiValue());
+			}
+		}
+		System.out.println();
+		Note[] array4 = composer4.composeCantusFirmus();
+		for(int x = 0; x < array4.length; x++) {
+			if(array4[x] != null) {
+				System.out.println(x + " " + array4[x].midiValue());
+			}
+		}
+		System.out.println();
+		Note[] array5 = composer5.composeCantusFirmus();
+		for(int x = 0; x < array5.length; x++) {
+			if(array5[x] != null) {
+				System.out.println(x + " " + array5[x].midiValue());
+			}
+		}
+		System.out.println();
+		Note[] array6 = composer6.composeCantusFirmus();
+		
+		for(int x = 0; x < array6.length; x++) {
+			if(array6[x] != null) {
+				System.out.println(x + " " + array6[x].midiValue());
+			}
 		}
 	}
 }
