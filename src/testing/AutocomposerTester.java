aba6eb3ac929d8a757787d32334cdda51bc51db9
Eric Ma <yma17@cranbrook.edu>
2016-07-22 19:07:56 -0400

    CORRECTION- Note constructors still do not work as intended
    Some issues regarding octaves -will fix later.

--- src/testing/AutocomposerTester.java
+++ src/testing/AutocomposerTester.java
@@ -13,23 +13,9 @@ public class AutocomposerTester implements NotesAndKeys
 
 	public static void main(String[] args)
 	{
-		//testing getSpecificArray - works
-		/*
-		Model m = new Model("A-sharp","Lydian",10); //F-flat should be E		
-
-		for(int x = 0; x < 7; x++)
-			System.out.println(m.getSpecificArray()[x]);
+		//testing Note class constructors - works
 		
-		*/
-		
-		//testing findPitch
-		/*
-		int pitch = NoteUtilities.findPitch("B",true);
-		System.out.println(pitch);
-		*/
-		
-		//testing Note class constructors
-		Model m = new Model("D", "Dorian",10);
+		Model m = new Model("G", "Phrygian",10);
 		int[] relPitchArray = {0,2,4,8,10,-8,-6,-4,-2,0};
 		Note[] noteArray = new Note[10];
 		noteArray[0] = new Note(m);
@@ -40,8 +26,22 @@ public class AutocomposerTester implements NotesAndKeys
 			System.out.println(noteArray[i].toString());
 		}
 		
+			
 		
+		//testing findPitch - works
+		/*
+		int pitch = NoteUtilities.findPitch("B",true);
+		System.out.println(pitch);
+		*/
 		
+		//testing getSpecificArray - works
+		/*
+		Model m = new Model("A-sharp","Lydian",10); //F-flat should be E		
+
+		for(int x = 0; x < 7; x++)
+			System.out.println(m.getSpecificArray()[x]);
+		
+		*/
 			
 	}
 }
